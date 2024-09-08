167aefea991bae78abbd601069bee133d4043b49
dansolovev <danya-solo@yandex.ru>
2024-09-08 05:23:16 +0300

    add tg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+'use client'
+import React, {useEffect} from "react";
 import BottomBar from "@/components/bottomBar";
 
 export default function Home() {
+    useEffect(() => {
+        window.Telegram.WebApp.ready();
+        window.Telegram.WebApp.expand();
+    })
+
   return (
       <div>
-          <script>
-              window.Telegram.WebApp.ready(); window.Telegram.WebApp.expand();
-          </script>
-        <h1 className="w-full capitalize text-4xl mx-auto text-center mt-10">Home Page</h1>
-        <BottomBar/>
+          <h1 className="w-full capitalize text-4xl mx-auto text-center mt-10">Home Page</h1>
+          <BottomBar/>
       </div>
   );
 }
